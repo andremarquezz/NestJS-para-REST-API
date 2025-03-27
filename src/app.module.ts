c9fb5787de257961de1460a261ff5e79fe5da791
Andre Marquez <andre.marqquez@gmail.com>
2025-03-27 19:57:07 -0300

    add: docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'db',
+      username: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true, // Don't use in production
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -1,8 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 export class Message {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ type: 'varchar', length: 255 })
   content: string;
   sender: string;
   recipient: string;
-  createdAt: Date;
+  @CreateDateColumn()
+  createdAt?: Date; // createdAt
+
+  @UpdateDateColumn()
+  updatedAt?: Date; // updatedAt
+  @Column({ default: false })
   read: boolean;
 }
